feb23f47e7cac2ac2d261c66c5ae3009f5795a51
nemetha17 <47603257+nemetha17@users.noreply.github.com>
2021-02-09 21:07:40 +0100

    Revert "Unite osztály és konstruktor"
    This reverts commit 0dabf40a8e9b65b1df462f8b3a5bd670d520fe86.

---
+++ Unite.java
@@ -0,0 +1,13 @@
+public class Unite(
+    int dmg;
+    int hp;
+
+    public Unite (int dmg, int hp) {
+        this.dmg=dmg;
+        this.hp=hp;
+    }
+
+    public void attack (Unite enemy){
+        enemy.hp=enemy.hp-this.dmg;
+    }
+)
