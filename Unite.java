0cbaa3e46594e74c3e17359a9b8f69262b8217d9
phollaki <phollakos15@gmail.com>
2021-02-11 11:11:22 +0100

    Életben van e függvény + védekezés hozzáadása

--- Unite.java
+++ Unite.java
@@ -1,14 +1,22 @@
 public class Unite {
     int dmg;
     int hp;
+    int def;
     
-    public Unite (int dmg, int hp){
+    public Unite (int dmg, int def, int hp){
         this.dmg=dmg;
+        this.def=def;
         this.hp=hp;
-
     }
 
     public void attack(Unite enemy){
-        enemy.hp=enemy.hp-this.dmg;
+        enemy.hp=enemy.hp+enemy.def-this.dmg;
+    }
+
+    public boolean isAlive(){
+        if(this.hp > 0){
+            return true;
+        }
+        return false;
     }
 }
