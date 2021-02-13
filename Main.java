329cd5b54f67a939a4bc5b10ddafca5e9dbd1944
nemethdav <60889598+nemethdav@users.noreply.github.com>
2021-02-13 19:52:15 +0100

    Main kész - Feladat megoldva

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String args[]) {
-        System.out.println("A Sasok megindultak!");
+        Unite our = new Unite(5, 6, 8);
+        Unite enemy = new Unite(8, 8, 10);
+        our.attack(enemy);
+        if (our.isAlive()){
+            System.out.println("Tuleltuk! :)");
+        } else {
+            System.out.println("Meghaltunk. :(");
+        }
     }
 }
