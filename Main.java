3b36ebba878090752d003d29243a8b4d1fa382ad
phollaki <phollakos15@gmail.com>
2021-02-14 11:34:49 +0100

    Unite-->Unit

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String args[]) {
-        Unite our = new Unite(5, 6, 8);
-        Unite enemy = new Unite(8, 8, 10);
+        Unit our = new Unit(5, 6, 8);
+        Unit enemy = new Unit(8, 8, 10);
         our.attack(enemy);
         System.out.println((our.isAlive()) ? "Tuleltuk! :)" : "Meghaltunk! :(");
     }
--- Unit.java
+++ Unit.java
@@ -1,15 +1,15 @@
-public class Unite {
+public class Unit {
     int dmg;
     int hp;
     int def;
     
-    public Unite (int dmg, int def, int hp){
+    public Unit (int dmg, int def, int hp){
         this.dmg=dmg;
         this.def=def;
         this.hp=hp;
     }
 
-    public void attack(Unite enemy){
+    public void attack(Unit enemy){
         if(enemy.isAlive()){
             if(enemy.def<this.dmg){
                 enemy.hp=enemy.hp+enemy.def-this.dmg;
