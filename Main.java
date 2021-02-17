70f3b257ebd13a373e91ed18946b365b9dc753a8
phollaki <phollakos15@gmail.com>
2021-02-17 18:49:09 +0100

    Hp és Dmg bekérése Scanner-el

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
-        Unit our = new Unit(5, 8);
-        Unit enemy = new Unit(8, 10);
-        our.attack(enemy);
-        System.out.println((our.isAlive()) ? "Tuleltuk! :)" : "Meghaltunk! :(");
-    }
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Harcosok élete:");
+        int warriorHp = sc.nextInt();
+        System.out.println("Harcosok sebzése: ");
+        int  warriorDmg = sc.nextInt();
+        System.out.println("Sámánok élete:");
+        int shamanHp = sc.nextInt();
+        System.out.println("Sámánok sebzése: ");
+        int  shamanDmg = sc.nextInt();
+        Unit warrior = new Unit(warriorDmg, warriorHp);
+        Unit shaman = new Unit(shamanDmg, shamanHp);
+        System.out.println("Warrior élete és sebzése: "+ warrior.hp +", "+ warrior.dmg + " || Shaman élete és sebzése: " + shaman.hp + ", " + shaman.dmg);
+    } 
 }
