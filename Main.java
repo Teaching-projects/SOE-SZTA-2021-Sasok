3469f34dc791b36aba25eb925aead64216bcfa5f
phollaki <phollakos15@gmail.com>
2021-02-28 12:48:42 +0100

    Bekérés és csata függvény módosítása #3

--- Main.java
+++ Main.java
@@ -2,33 +2,46 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Harcosok élete:");
-        int warriorHp = sc.nextInt();
-        System.out.println("Harcosok sebzése: ");
-        int  warriorDmg = sc.nextInt();
-        System.out.println("Sámánok élete:");
-        int shamanHp = sc.nextInt();
-        System.out.println("Sámánok sebzése: ");
-        int  shamanDmg = sc.nextInt();
-        Unit warrior = new Unit(warriorDmg, warriorHp);
-        Unit shaman = new Unit(shamanDmg, shamanHp);
-        System.out.println("Warrior élete és sebzése: "+ warrior.hp +", "+ warrior.dmg + " || Shaman élete és sebzése: " + shaman.hp + ", " + shaman.dmg + '\n');
-        battle(warrior,shaman);
-        sc.close();
-    } 
+        initialize();
+    }
 
-    private static void battle (Unit unit1, Unit unit2){
+    private static void battle(Unit unit1, Unit unit2) {
+        System.out.println("A Csata elkezdődött! " + unit1.name + " kezdheti a támadást " + unit2.name + (" ellen."));
         int i = 1;
-        while(unit1.isAlive() && unit2.isAlive()){
+        while (unit1.isAlive() && unit2.isAlive()) {
             unit1.attack(unit2);
-            System.out.println(i + ". körben az első egység élete: " + unit1.hp + ", a második egység élete: " + unit2.hp);
+            System.out.println(
+                    i + ". körben " + unit1.name + " élete: " + unit1.hp + " || " + unit2.name + " élete: " + unit2.hp);
             i++;
             unit2.attack(unit1);
-            System.out.println(i + ". körben az első egység élete: " + unit1.hp + ", a második egység élete: " + unit2.hp);
+            System.out.println(
+                    i + ". körben " + unit1.name + " élete: " + unit1.hp + " || " + unit2.name + " élete: " + unit2.hp);
             i++;
         }
-        System.out.println(unit1.isAlive() ? unit1 + " nyert" : unit2 +" nyert");
+        System.out.println(unit1.isAlive() ? "\n" + unit1.name.toUpperCase() + " GYŐZEDELMESKEDETT!"
+                : "\n" + unit2.name.toUpperCase() + " GYŐZEDELMESKEDETT!");
+    }
+
+    private static void initialize() {
+        Scanner sc = new Scanner(System.in);
+        // Első Unit
+        System.out.println("Első egység neve: ");
+        String unit1Name = sc.next();
+        System.out.println(unit1Name + " élete:");
+        int unit1Hp = sc.nextInt();
+        System.out.println(unit1Name + " sebzése: ");
+        int unit1Dmg = sc.nextInt();
+        // Második Unit
+        System.out.println("Második egység neve: ");
+        String unit2Name = sc.next();
+        System.out.println(unit2Name + " élete:");
+        int unit2Hp = sc.nextInt();
+        System.out.println(unit2Name + " sebzése: ");
+        int unit2Dmg = sc.nextInt();
+        // Két Unit létrehozása
+        Unit unit1 = new Unit(unit1Name, unit1Dmg, unit1Hp);
+        Unit unit2 = new Unit(unit2Name, unit2Dmg, unit2Hp);
+        sc.close();
+        battle(unit1, unit2);
     }
 }
--- Unit.java
+++ Unit.java
@@ -1,19 +1,22 @@
 public class Unit {
+    String name;
     final int dmg;
     int hp;
-    
-    public Unit (int dmg, int hp){
-        this.dmg=dmg;
-        this.hp=hp;
+
+    public Unit(String name, int dmg, int hp) {
+        this.name = name;
+        this.dmg = dmg;
+        this.hp = hp;
     }
 
-    public void attack(Unit enemy){
-        if(enemy.isAlive() && this.isAlive()){
-                enemy.hp -= this.dmg;  
+    public void attack(Unit enemy) {
+        if (enemy.isAlive() && this.isAlive()) {
+            enemy.hp -= this.dmg;
         }
     }
 
-    public boolean isAlive(){
+    public boolean isAlive() {
         return this.hp > 0;
     }
+
 }
