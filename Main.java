ff4d1401832e9cb7464336d6919ad799ee00ee09
phollaki <phollakos15@gmail.com>
2021-03-09 09:20:43 +0100

    Első ütés után győzhet valaki

--- Main.java
+++ Main.java
@@ -13,8 +13,8 @@ public class Main {
         int unit2As = readIn(sc, "Második egység AS-je: ");
 
         // Két Unit létrehozása
-        Unit unit1 = new Unit("Warrior", unit1Dmg, unit1Hp, unit1As);
-        Unit unit2 = new Unit("Shaman", unit2Dmg, unit2Hp, unit2As);
+        Unit unit1 = new Unit("Harcos", unit1Dmg, unit1Hp, unit1As);
+        Unit unit2 = new Unit("Sámán", unit2Dmg, unit2Hp, unit2As);
 
         sc.close();
 
@@ -43,20 +43,25 @@ public class Main {
         String name1 = unit1.getName();
         String name2 = unit2.getName();
         boolean bothAlive = true;
-        // Mindkettő megüti egymást elősször
-        unit1.attack(unit2);
-        unit2.attack(unit1);
-        System.out.println("\nA Csata elkezdődött! " + name1 + " és " + name2 + " megtámadták egymást. Életük: " + name1
-                + " " + unit1.getHP() + ", " + name2 + " " + unit2.getHP());
+
         // Amíg mindkettő él
-        int i = 0;
+        int i = -1;
         while (bothAlive) {
             i++;
-            if (i % as1 == 0) {
+            // Egyszerre megütik egymást 1. ütés!
+            if (i <= 0) {
+                unit1.attack(unit2);
+                unit2.attack(unit1);
+                System.out.println("\nA Csata elkezdődött! " + name1 + " és " + name2 + " megtámadták egymást. Életük: "
+                        + name1 + " " + unit1.getHP() + ", " + name2 + " " + unit2.getHP());
+            }
+            // Ha túlélték akkor ütnek amikor lejár az Attackspeed pl.: attackspeed = 1,
+            // akkor minden i-nél üthetünk (1i = 1mp)
+            if (i > 0 && i % as1 == 0) {
                 unit1.attack(unit2);
                 System.out.println(name1 + " megtámadta " + name2 + ", így " + name2 + " élete - " + unit2.getHP());
             }
-            if (i % as2 == 0) {
+            if (i > 0 && i % as2 == 0) {
                 unit2.attack(unit1);
                 System.out.println(name2 + " megtámadta " + name1 + ", így " + name1 + " élete - " + unit1.getHP());
             }
@@ -64,7 +69,11 @@ public class Main {
                 bothAlive = false;
             }
         }
-        System.out.println(unit1.isAlive() ? "\n" + unit1.getName().toUpperCase() + " GYŐZEDELMESKEDETT!"
-                : "\n" + unit2.getName().toUpperCase() + " GYŐZEDELMESKEDETT!");
+        if (i <= 0)
+            System.out.println(unit1.isAlive() ? "\n" + unit1.getName().toUpperCase() + " EGY CSAPÁSSAL GYŐZÖTT"
+                    : "\n" + unit2.getName().toUpperCase() + " EGY CSAPÁSSAL GYŐZÖTT!");
+        else
+            System.out.println(unit1.isAlive() ? "\n" + unit1.getName().toUpperCase() + " GYŐZEDELMESKEDETT!"
+                    : "\n" + unit2.getName().toUpperCase() + " GYŐZEDELMESKEDETT!");
     }
 }
